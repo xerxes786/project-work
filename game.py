af9a1342c0fdaa814d7a93e2ce1e976bb3a7c9ed
xerxes786 <71205360+xerxes786@users.noreply.github.com>
2020-09-13 11:54:39 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the terms"))
+b=0                                         
+c=1                                         
+if a<=0:
+    print("The requested series needs to be positive")
+else:
+    print(b)
+    print(c)
+    for i in range(2,a):
+        d= b + c                          
+        b=c
+        c=d
+        print(d)
